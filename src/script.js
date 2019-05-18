a1258b5ce74055c004cd386ab2b4bbe110fb4748
ibbi <hi@ibbi.io>
2019-05-18 16:14:23 -0700

    wpo

--- src/script.js
+++ src/script.js
@@ -1,10 +1,19 @@
 // import data from './resources/dictionary.js'
 
-var data = require('./resources/dictionary.js')
-let i = 0
-let array = Object.keys(data)
-while (i<array.length) {
-    if (data.hasOWn)
-    console.log(array[i]);
-    ++i;
+var data = require('./resources/dictionary.js');
+let i = 0;
+let array = Object.keys(data);
+// while (i < array.length) {
+// 	if (data.hasOwnProperty(array[i])) {
+// 		let defArray = data[array[i]].split(' ');
+// 	}
+// 	console.log(array[i]);
+// 	++i;
+// }
+let defString = data[array[6]];
+let cleanString = defString.replace(/[^a-z ]/gi, '');
+
+if (data.hasOwnProperty(array[6])) {
+	let defArray = cleanString.toLowerCase().split(' ');
+	console.log(array[6], defArray);
 }
