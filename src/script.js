52f28e735a252fc1af814a758a9a07078ebb9925
ibbi <hi@ibbi.io>
2019-05-18 16:02:31 -0700

    thing

---
+++ src/script.js
@@ -0,0 +1,10 @@
+// import data from './resources/dictionary.js'
+
+var data = require('./resources/dictionary.js')
+let i = 0
+let array = Object.keys(data)
+while (i<array.length) {
+    if (data.hasOWn)
+    console.log(array[i]);
+    ++i;
+}
