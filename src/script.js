e2ffd57d85393ea2f03cfdf16d1d5160fb8b45f9
ibbi <hi@ibbi.io>
2019-05-18 16:21:22 -0700

    init

--- src/script.js
+++ src/script.js
@@ -3,17 +3,21 @@
 var data = require('./resources/dictionary.js');
 let i = 0;
 let array = Object.keys(data);
-// while (i < array.length) {
-// 	if (data.hasOwnProperty(array[i])) {
-// 		let defArray = data[array[i]].split(' ');
-// 	}
-// 	console.log(array[i]);
-// 	++i;
-// }
+
 let defString = data[array[6]];
 let cleanString = defString.replace(/[^a-z ]/gi, '');
 
 if (data.hasOwnProperty(array[6])) {
 	let defArray = cleanString.toLowerCase().split(' ');
-	console.log(array[6], defArray);
+	defArray.map((i) => {
+		if (data.hasOwnProperty(i)) {
+			console.log(
+				'--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------',
+				i,
+				data[i]
+			);
+		}
+	});
+
+	// console.log(array[6], defArray);
 }
